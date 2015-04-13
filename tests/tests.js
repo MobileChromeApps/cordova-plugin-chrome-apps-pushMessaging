2b747ff6369de76aaf067e23dae04e061ded6853
Jason Chase <jasonpchase@gmail.com>
2015-04-13 15:30:22 -0400

    Fix references specific to ChromeSpec - Use console.log instead of `logger` from ChromeSpec app

--- tests/tests.js
+++ tests/tests.js
@@ -3,24 +3,24 @@
 // found in the LICENSE file.
 
 exports.defineManualTests = function(rootEl, addButton) {
-  logger('This test requires a physical device (not an emulator)');
-  logger('Please ensure that oauth2 is configured for chrome.identity');
-  logger('See the README for details');
+  console.log('This test requires a physical device (not an emulator)');
+  console.log('Please ensure that oauth2 is configured for chrome.identity');
+  console.log('See the README for details');
 
   // We store the channel and registration ids.
   var pushMessagingIds;
 
   // Add a listener.
   chrome.pushMessaging.onMessage.addListener(function(message) {
-    logger('Push message arrived. Subchannel: ' + message.subchannelId + ', message: "' + JSON.parse(message.payload).message + '"');
+    console.log('Push message arrived. Subchannel: ' + message.subchannelId + ', message: "' + JSON.parse(message.payload).message + '"');
   });
 
   addButton('Get Channel Id', function() {
     var onGetChannelIdSuccess = function(channelId) {
       pushMessagingIds = channelId;
-      logger('Chrome Channel id: ' + pushMessagingIds.channelId);
-      logger('Android Registration id: ' + pushMessagingIds.registrationId);
-      logger('APNS Device Token: ' + pushMessagingIds.deviceToken);
+      console.log('Chrome Channel id: ' + pushMessagingIds.channelId);
+      console.log('Android Registration id: ' + pushMessagingIds.registrationId);
+      console.log('APNS Device Token: ' + pushMessagingIds.deviceToken);
     };
 
     chrome.pushMessaging.getChannelId(true /* interactive */, onGetChannelIdSuccess);
@@ -28,11 +28,11 @@ exports.defineManualTests = function(rootEl, addButton) {
 
   addButton('Send Push Message via Android', function() {
     if (!pushMessagingIds || !pushMessagingIds.registrationId) {
-      logger('No registration id!');
+      console.log('No registration id!');
       return;
     }
 
-    logger('Sending push message...');
+    console.log('Sending push message...');
 
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://android.googleapis.com/gcm/send');
@@ -41,23 +41,23 @@ exports.defineManualTests = function(rootEl, addButton) {
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
         if (xhr.status < 200 || xhr.status > 300) {
-          logger('Android GCM push message request failed with status ' + xhr.status + '.');
+          console.log('Android GCM push message request failed with status ' + xhr.status + '.');
         }
       }
     };
     var data = {'registration_ids' : [ pushMessagingIds.registrationId ], 'data' : { 'message' : 'Push message sent via Android GCM.' } };
     xhr.send(JSON.stringify(data));
 
-    logger('...push message sent.');
+    console.log('...push message sent.');
   });
 
   addButton('Send Push Message via Chrome', function() {
     if (!pushMessagingIds || !pushMessagingIds.channelId) {
-      logger('No channel id!');
+      console.log('No channel id!');
       return;
     }
 
-    logger('Sending push message...');
+    console.log('Sending push message...');
 
     var getAuthTokenCallback = function(token) {
       var xhr = new XMLHttpRequest();
@@ -67,8 +67,8 @@ exports.defineManualTests = function(rootEl, addButton) {
       xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
           if (xhr.status < 200 || xhr.status > 300) {
-            logger('Chrome GCM push message request failed with status ' + xhr.status + '.');
-            logger('Response text: ' + xhr.responseText);
+            console.log('Chrome GCM push message request failed with status ' + xhr.status + '.');
+            console.log('Response text: ' + xhr.responseText);
           }
         }
       };
@@ -77,7 +77,7 @@ exports.defineManualTests = function(rootEl, addButton) {
                    'payload' : JSON.stringify({ 'message' : 'Push message sent via Chrome GCM.' }) };
       xhr.send(JSON.stringify(data));
 
-      logger('...push message sent.');
+      console.log('...push message sent.');
     };
 
     chrome.identity.getAuthToken({ interactive: true }, getAuthTokenCallback);
